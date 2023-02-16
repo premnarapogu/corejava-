7968440da87df7b3c859003c932fdeac84b314c0
premnarapogu <92223579+premnarapogu@users.noreply.github.com>
2023-02-16 21:32:38 +0530

    Add files via upload

---
+++ PrimitiveDatatypes.java
@@ -0,0 +1,20 @@
+package kumar;
+
+public class PrimitiveDatatypes {
+
+	public static void main(String[] args) {
+		
+		byte b1 = 5;
+		short s1 = 20;
+		int a  = 100;
+		long b = 999L;
+		boolean c = true ;
+		 System.out.println(b1);
+		 System.out.println(s1);
+		 System.out.println(a);
+		 System.out.println(b);
+		 System.out.println("true");
+	   
+	}
+
+}

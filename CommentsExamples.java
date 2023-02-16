6baa5d506d27ddaecc4388b2d603d122003abe40
premnarapogu <92223579+premnarapogu@users.noreply.github.com>
2023-02-16 20:46:00 +0530

    Add files via upload

---
+++ CommentsExamples.java
@@ -0,0 +1,12 @@
+package kumar;
+
+//single line comments example
+public class CommentsExamples {    
+    public static void main(String[] args) {    
+    int i=10; // i is a variable with value 10  
+System.out.println(i); //printing the variable i 
+
+
+    }    
+}    
+
---
+++ Welcome.java
@@ -0,0 +1,7 @@
+package kumar;
+
+public class Welcome {
+	public static void main(String[] args) {
+		System.out.println("welcome to the world of java");
+	}
+}

2e840b76eb249ddd83bdde9452b912d40190a8bd
premnarapogu <92223579+premnarapogu@users.noreply.github.com>
2023-02-20 06:57:40 +0530

    Assignments2.java

---
+++ FlowerExercise5.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Flowexercise5 {
+
+	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int i, n = 0, s = 0;
+		double avg;
+		{
+
+			System.out.println("Input the 5 numbers : ");
+
+		}
+		for (i = 0; i < 5; i++) {
+
+			n = in.nextInt();
+
+			s += n;
+		}
+		avg = s / 5;
+		System.out.println("The sum of 5 no is : " + s + "\nThe Average is : " + avg);
+	}
+
+}

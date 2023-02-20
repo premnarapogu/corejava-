8c91204bee77528e65ae660cb3a109378934ad2d
premnarapogu <92223579+premnarapogu@users.noreply.github.com>
2023-02-20 06:55:43 +0530

    Assignments2.java

---
+++ FlowerExercise4.java
@@ -0,0 +1,13 @@
+public class Flowexercise4 {
+
+	public static void main(String[] args) {
+		int a;
+		System.out.println("the first 10 natural numbers are");
+		for (a=1;a<=10;a++)
+		{
+			System.out.println(a);
+		}
+
+	}
+
+}
